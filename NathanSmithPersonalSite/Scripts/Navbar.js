7260a4b08d2a40d3e7fcac6f1a606fe2ebf33fdf
Nathan Smith <ns23@umail.iu.edu>
2018-02-12 11:51:24 -0500

    Use ViewBag to eliminate second layout page, Add background color to social media icons for index page, Fix double-clicking collapsable navbar

--- NathanSmithPersonalSite/Scripts/Navbar.js
+++ NathanSmithPersonalSite/Scripts/Navbar.js
@@ -1,19 +1,11 @@
 ﻿//TODO: look at data attributes
-//look up cyclomatic complexity
-//change conditional to check for positive
-//look for jquery toggle
-//look up id convention
 //look up kill biding to button click
-$('.navbar-toggle').click(function () {
+function moveMediaBar() {
     if ($(window).width() >= 600) {
         $('#social-media-bar').hide().toggleClass('sticky-container-low').fadeIn(650);
+        $('.navbar-toggle').off();
+        setTimeout(function () { $('.navbar-toggle').on("click", moveMediaBar); }, 400);
     }
-    //if ($(window).width() <= 600) //The social media bar does not need to move
-    //    return;
-    //if ($('#social-media-bar').hasClass('sticky-container-low')) {
-    //    $('#social-media-bar').hide().removeClass('sticky-container-low').fadeIn(600);
-    //}
-    //else {
-    //    $('#social-media-bar').hide().addClass('sticky-container-low').fadeIn(700);
-    //}
-});
+}
+
+$('.navbar-toggle').click(moveMediaBar);
